6ed7ba357b260ece45664121c8a921c78be83f5e
raventu <80778915raventu@gmail.com>
2023-08-02 17:29:03 +0800

    feat: nuxt wss 禁用广播

--- server/plugins/wsServer.ts
+++ server/plugins/wsServer.ts
@@ -2,13 +2,12 @@
  * @Author: raventu
  * @Date: 2023-06-30 13:40:45
  * @LastEditors: raventu
- * @LastEditTime: 2023-07-28 17:55:55
+ * @LastEditTime: 2023-08-02 10:54:34
  * @FilePath: /cq-green-magpies-app/server/plugins/wsServer.ts
  * @Description: 启动 ws 服务
  */
 import { WebSocketServer } from 'ws'
 import jwt from 'jsonwebtoken'
-import { WSLogs } from '~/server/utils/helper/logsHelper'
 
 export default defineNitroPlugin(() => {
   const wsServerPort = useAppConfig().wsServerPort
@@ -34,29 +33,12 @@ export default defineNitroPlugin(() => {
           return false
         }
       }
-
       return !!user
     },
   })
 
   wss.on('connection', (ws) => {
     ws.on('error', console.error)
-    // 客户端
-    ws.on('message', (data) => {
-      // 去掉转义字符
-      const s = data.toString()
-      WSLogs(s)
-      // 服务器广播
-      // 广播到所有连接的WebSocket客户端，包括其自身。
-      wss.clients.forEach((client) => {
-        // 判断当前连接服务端的所有客户端的ws用户。
-        // client!==ws 表示不是当前发送的用户[不要自己广播自己]
-        if (client !== ws) {
-          // binary表示数据是否是二进制。binary:false表示不是二进制
-          client.send(data, { binary: false })
-        }
-      })
-    })
     // 第一次连接成功后发送的消息。
     const sendObj = { message: 'connection successful', retCode: 200, userAgent: 'nuxt-server' }
     ws.send(JSON.stringify(sendObj))
