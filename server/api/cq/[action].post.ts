e75165e8c8ede0b0e0c1ab52acfcfdcf9120a5c1
raventu <80778915raventu@gmail.com>
2023-08-02 14:48:10 +0800

    refactor: 重构 CQ APi 流程

--- server/api/cq/[action].post.ts
+++ server/api/cq/[action].post.ts
@@ -2,17 +2,14 @@
  * @Author: raventu
  * @Date: 2023-06-27 18:11:26
  * @LastEditors: raventu
- * @LastEditTime: 2023-07-25 11:28:59
+ * @LastEditTime: 2023-08-02 14:47:42
  * @FilePath: /cq-green-magpies-app/server/api/cq/[action].post.ts
- * @Description:
+ * @Description: 处理 cq api 请求
  */
-import WebSocket from 'ws'
 import Joi from 'joi'
-import { genEchoStr, responseObject } from '~/server/utils/helper'
+import { responseObject } from '~/server/utils/helper'
 
 export default defineEventHandler(async (event) => {
-  // 请求头中获取  accessToken
-  const reqAccessToken = getHeader(event, 'Authorization')
   const actionName = getRouterParam(event, 'action')
   const body = await readBody(event)
   // 校验数据joi
@@ -27,42 +24,24 @@ export default defineEventHandler(async (event) => {
   catch (err: any) {
     return responseObject(400, err.stack, {})
   }
-  const wsQuery = () => {
-    const accessToken = ((reqAccessToken ?? '').replace('Bearer ', ''))
-    return `?accessToken=${accessToken}`
-  }
-  const wsPath = `ws://127.0.0.1:4000/${wsQuery()}`
-  const ws = new WebSocket(wsPath)
-  const echoStr = genEchoStr()
+  const botInstance = globalThis.cqBot
+  if (!botInstance)
+    return responseObject(400, 'CQ API not connected', {})
 
   return new Promise((resolve) => {
-    // 发送实际  websocket 请求
-    ws.on('open', () => {
-      ws.on('message', (msg) => {
-        try {
-          const { echo, message, status, data } = JSON.parse(msg.toString())
-          if (echo === echoStr) {
-            ws.close()
-            return resolve(responseObject(200, `${status}-${message}`, data))
-          }
-        }
-        catch (e) {
-          return resolve(responseObject(400, e as string, {}))
-        }
+    //  超时
+    setTimeout(() => {
+      ws.close()
+      return resolve(responseObject(400, 'CQ API time out', {}))
+    }, 5000)
+
+    botInstance(action, params)
+      .then((res) => {
+        const { retcode, status, data } = res
+        return resolve(responseObject(200, `${status}-${retcode}`, data as any))
       })
-
-      //  超时
-      setTimeout(() => {
-        ws.close()
-        return resolve(responseObject(400, 'CQ API time out', {}))
-      }, 5000)
-
-      ws.send(JSON.stringify({
-        action,
-        params,
-        echo: echoStr,
-        from: 'nt-server',
-      }))
-    })
+      .catch((err) => {
+        return resolve(responseObject(400, err as string, {}))
+      })
   })
 })
