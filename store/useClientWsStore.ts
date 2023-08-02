1d6da260ec22140f3a50f0567a7db251635ffac3
raventu <80778915raventu@gmail.com>
2023-08-02 18:04:33 +0800

    feat: useClientWsStore 修正 clientWs 类型

--- store/useClientWsStore.ts
+++ store/useClientWsStore.ts
@@ -2,11 +2,12 @@
  * @Author: raventu
  * @Date: 2023-07-14 09:59:37
  * @LastEditors: raventu
- * @LastEditTime: 2023-07-25 11:04:51
+ * @LastEditTime: 2023-08-02 17:59:29
  * @FilePath: /cq-green-magpies-app/store/useClientWsStore.ts
  * @Description: 客户端 ws store
  */
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import type { UseWebSocketReturn } from '@vueuse/core'
 import { useWebSocket } from '@vueuse/core'
 
 export const useClientWsStore = defineStore('clientWsStore', () => {
@@ -18,8 +19,13 @@ export const useClientWsStore = defineStore('clientWsStore', () => {
     const accessToken = useUserStore().getAccessToken()
     return `?accessToken=${accessToken}`
   })
-  const clientWs = ref<ReturnType<typeof useWebSocket> | null>(null)
-  const clientWsInstance = computed(() => clientWs.value?.ws)
+  const clientWs = ref<UseWebSocketReturn<any> >()
+  const clientWsInstance = computed(() => unref(unref(clientWs)?.ws))
+  const clientWsStatus = computed(() => unref(unref(clientWs)?.status))
+  const clientWsData = computed(() => unref(unref(clientWs)?.data))
+  const clientWsSend = computed(() => (unref(clientWs)?.send))
+  const clientWsOpen = computed(() => (unref(clientWs))?.open)
+  const clientWsClose = computed(() => (unref(clientWs)?.close))
 
   const initClientWs = () => {
     const ws = useWebSocket(wsPath.value + wsQuery.value, {
@@ -27,8 +33,7 @@ export const useClientWsStore = defineStore('clientWsStore', () => {
         connectFlag.value = true
       },
     })
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
+
     clientWs.value = ws
   }
 
@@ -43,6 +48,11 @@ export const useClientWsStore = defineStore('clientWsStore', () => {
     clientWsInstance,
     connectFlag,
     initClientWs,
+    clientWsStatus, //  ws 状态
+    clientWsData, //  ws 数据
+    clientWsSend, //  ws 发送
+    clientWsOpen, //  ws 打开
+    clientWsClose, // ws 关闭
   }
 })
 
