92d187f8eca1441fd8956bba13beb86f621fec3c
raventu <80778915raventu@gmail.com>
2023-08-22 15:35:21 +0800

    style: 改进实时日志样式

--- components/Sys/debug.vue
+++ components/Sys/debug.vue
@@ -2,16 +2,18 @@
  * @Author: raventu
  * @Date: 2023-07-05 13:29:23
  * @LastEditors: raventu
- * @LastEditTime: 2023-08-02 17:58:16
+ * @LastEditTime: 2023-08-22 14:42:44
  * @FilePath: /cq-green-magpies-app/components/Sys/debug.vue
  * @Description:  系统-调试
 -->
 <script lang='ts' setup>
 import { storeToRefs } from 'pinia'
+import type { LogInst } from 'naive-ui'
 import { useClientWsStore } from '~/store/useClientWsStore'
 
 const { clientWsInstance } = storeToRefs(useClientWsStore())
 const logs = ref<string[]>([])
+const logInstRef = ref<LogInst | null>(null)
 
 onMounted(async () => {
   await nextTick()
@@ -19,7 +21,13 @@ onMounted(async () => {
   if (instance) {
     instance.onmessage = (event: MessageEvent) => {
       console.log('【调试】', event.data)
-      logs.value.push(`【${new Date().toLocaleString()}】\n ${event.data}`)
+      logs.value.push(`【${new Date().toLocaleString()}】\n ${event.data} \n`)
+      // 滚动到底部
+      if (logInstRef.value) {
+        nextTick(() => {
+          logInstRef.value?.scrollTo({ position: 'bottom', slient: true })
+        })
+      }
     }
   }
 })
@@ -27,12 +35,6 @@ onMounted(async () => {
 
 <template>
   <ClientOnly>
-    <n-card title="日志" class="h-full">
-      <div class="h-70vh w-full flex flex-col overflow-y-scroll space-y-2">
-        <p v-for="(item, index) in logs" :key="index">
-          {{ item }}
-        </p>
-      </div>
-    </n-card>
+    <n-log ref="logInstRef" :lines="logs" :rows="37" trim class="h-full space-y-2" />
   </ClientOnly>
 </template>
